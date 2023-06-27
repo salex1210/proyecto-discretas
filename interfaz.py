938116ec4cd954cb33136b405f542f7974a25569
Gerson Alexander Sanchez Brito <sanchezbritog@gmail.com>
2023-06-26 22:04:08 -0500

    musica

--- interfaz.py
+++ interfaz.py
@@ -1,17 +1,24 @@
 from tkinter import *
 from PIL import Image, ImageTk
+import pygame
 
 def mensaje():
     print("Hola mundo")
 
 ventana = Tk() #creando el objeto ventana a partir del contructor
 
+# Inicializar pygame
+pygame.mixer.init()
 
+# Cargar la música de fondo
+pygame.mixer.music.load("Raiders March.mp3")
+
+# Reproducir la música en bucle
+pygame.mixer.music.play(-1)
 
 #imagen =  PhotoImage(file = r'C:\Users\GERSONSANCHEZ\OneDrive - UNIVERSIDAD INDUSTRIAL DE SANTANDER\Documents\2023-1\TRATAMIENTO DE SEñALES DISCRETAS O1\proyecto-discretas\fondR.png')
 imagen = Image.open('fond0.png')
 imagen = imagen.resize((1030,600), Image.ANTIALIAS)
-
 
 
 img = ImageTk.PhotoImage(imagen)
@@ -20,7 +27,7 @@ fondo.place(x=0, y=0, relwidth=1, relheight=1)
 fondo.pack()
 
 botonStart = Button(ventana,text="START",width=20, height=4)
-botonStart.place(x=450,y=450)
+botonStart.place(x=425,y=500)
 
 #botonStart.pack()
 

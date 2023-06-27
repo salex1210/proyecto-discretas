88df2fb6fc60ae5b1bdf020415bd875db2901461
Gerson Alexander Sanchez Brito <sanchezbritog@gmail.com>
2023-06-26 20:43:47 -0500

    fondo con boton

--- interfaz.py
+++ interfaz.py
@@ -6,14 +6,25 @@ def mensaje():
 
 ventana = Tk() #creando el objeto ventana a partir del contructor
 
+
+
 #imagen =  PhotoImage(file = r'C:\Users\GERSONSANCHEZ\OneDrive - UNIVERSIDAD INDUSTRIAL DE SANTANDER\Documents\2023-1\TRATAMIENTO DE SEñALES DISCRETAS O1\proyecto-discretas\fondR.png')
 imagen = Image.open('fond0.png')
 imagen = imagen.resize((1030,600), Image.ANTIALIAS)
 
+
+
 img = ImageTk.PhotoImage(imagen)
 fondo = Label(ventana, image=img)
-fondo.pack()
 fondo.place(x=0, y=0, relwidth=1, relheight=1)
+fondo.pack()
+
+botonStart = Button(ventana,text="START",width=20, height=4)
+botonStart.place(x=450,y=450)
+
+#botonStart.pack()
+
+
 ventana.geometry("1030x600") #definiendo el tamaño de la ventana
 ventana.title("Home") #Nombre de la ventana
 

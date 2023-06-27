9b7761a4a1e3d0e73e4be6de292fc71b687419fe
Gerson Alexander Sanchez Brito <sanchezbritog@gmail.com>
2023-06-26 19:05:25 -0500

    avance hoy

--- interfaz.py
+++ interfaz.py
@@ -1,9 +1,21 @@
-from tkinter import Tk,Label,Button
+from tkinter import *
+from PIL import Image, ImageTk
 
 def mensaje():
     print("Hola mundo")
 
 ventana = Tk() #creando el objeto ventana a partir del contructor
-ventana.geometry("400x280") #definiendo el tamaño de la ventana
+
+#imagen =  PhotoImage(file = r'C:\Users\GERSONSANCHEZ\OneDrive - UNIVERSIDAD INDUSTRIAL DE SANTANDER\Documents\2023-1\TRATAMIENTO DE SEñALES DISCRETAS O1\proyecto-discretas\fondR.png')
+imagen = Image.open('fond0.png')
+imagen = imagen.resize((1030,600), Image.ANTIALIAS)
+
+img = ImageTk.PhotoImage(imagen)
+fondo = Label(ventana, image=img)
+fondo.pack()
+fondo.place(x=0, y=0, relwidth=1, relheight=1)
+ventana.geometry("1030x600") #definiendo el tamaño de la ventana
 ventana.title("Home") #Nombre de la ventana
+
+
 ventana.mainloop()
